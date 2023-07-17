ed6a7c5ea060ed761e2f56d70f9d20be431df8fa
Andrej Prskalo <aprskalo00@gmail.com>
2023-07-17 15:23:06 +0200

    minor tasks changes

--- crypto_scanner/tasks.py
+++ crypto_scanner/tasks.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, unicode_literals
-from django.db.models import Count
 from django.utils import timezone
 from crypto_scanner.models import (
     BtcKline1m,
@@ -17,7 +16,6 @@ from crypto_scanner.models import (
     MaticKline1m,
     AvaxKline1m,
     ShibKline1m,
-    BtcPrice,
 )
 from celery import shared_task
 from binance.client import Client
@@ -49,16 +47,21 @@ databaseModels = {
 
 
 @shared_task
-def fetch_all_1m_klines():
-    print("fetching all 1m klines")
-
+def fetch_all_1m_klines(limit=25):
     for ticker in databaseModels:
+        model = databaseModels[ticker]
         klines = client.get_klines(
-            symbol=ticker, interval=Client.KLINE_INTERVAL_1MINUTE, limit=11
+            symbol=ticker, interval=Client.KLINE_INTERVAL_1MINUTE, limit=limit
         )
 
+        kline_objects = []
         for kline in klines:
-            store_kline(ticker, kline)
+            kline_object = create_kline_object(ticker, kline)
+            if kline_object:
+                kline_objects.append(kline_object)
+
+        if kline_objects:
+            model.objects.bulk_create(kline_objects)
 
 
 def populate_all_klines_date(date):
@@ -67,22 +70,28 @@ def populate_all_klines_date(date):
 
 
 def populate_kline(ticker, date):
-    client = Client()
+    model = databaseModels[ticker]
 
     klines = client.get_historical_klines(ticker, Client.KLINE_INTERVAL_1MINUTE, date)
 
+    kline_objects = []
     for kline in klines:
-        store_kline(ticker, kline)
+        kline_object = create_kline_object(ticker, kline)
+        if kline_object:
+            kline_objects.append(kline_object)
+
+    if kline_objects:
+        model.objects.bulk_create(kline_objects)
 
 
-def store_kline(ticker, kline):
+def create_kline_object(ticker, kline):
     start_time = timezone.make_aware(
         datetime.fromtimestamp(kline[0] / 1000), timezone.utc
     )
 
     model = databaseModels[ticker]
     if model.objects.filter(start_time=start_time).exists():
-        return
+        return None
 
     end_time = timezone.make_aware(
         datetime.fromtimestamp(kline[6] / 1000), timezone.utc
@@ -102,4 +111,21 @@ def store_kline(ticker, kline):
         taker_buy_quote_asset_volume=kline[10],
     )
 
-    kline_obj.save()
+    return kline_obj
+
+
+# BCHUSDT
+# AVAXUSDT
+# SHIBUSDT
+# MATICUSDT
+# LINKUSDT
+# LTCUSDT
+# UNIUSDT
+# DOGEUSDT
+# DOTUSDT
+# ADAUSDT
+# SOLUSDT
+# BNBUSDT
+# XRPUSDT
+# ETHUSDT
+# BTCUSDT
