72a3b5e661bb6a5444d7485290f5a0c8fdf8bc44
Andrej Prskalo <aprskalo00@gmail.com>
2023-08-02 15:43:42 +0200

    added django cors headers

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "crypto_scanner",
     "rest_framework",
 ]
@@ -62,6 +63,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://crypto-scanner-phi.vercel.app",
 ]
 
 REST_FRAMEWORK = {
