00540c8a4b187e22b11a057abb363a944df5e04b
Andrej Prskalo <aprskalo00@gmail.com>
2023-08-09 22:07:34 +0200

    removed / from urls

--- crypto_scanner/urls.py
+++ crypto_scanner/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from crypto_scanner import views
 
 urlpatterns = [
-    path("snippets/", views.snippet_list),
-    path("snippets/<int:pk>/", views.snippet_detail),
+    path("snippets", views.snippet_list),
+    path("snippets/<int:pk>", views.snippet_detail),
     path(
-        "average-price/<str:symbol>/<str:duration>/",
+        "average-price/<str:symbol>/<str:duration>",
         views.average_price_change_per_day_of_week,
         name="average-price-change-per-week",
     ),
-    path("average-price/select/", views.average_price_change_per_day_of_week_select),
+    path("average-price/select", views.average_price_change_per_day_of_week_select),
     path(
         "pearson-correlation/<str:duration>",
         views.get_pearson_correlation,
