856ea6b8f9c0a74f7a75ff29a60848eecd93eb0a
aprs00 <ploutos@DESKTOP-LQ6OCIF>
2024-05-12 14:44:21 +0200

    Invoke klines main

--- exchange_connections/apps.py
+++ exchange_connections/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+from exchange_connections.binance.klines import main as start_binance_klines
+
 
 class ExchangeConnectionsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "exchange_connections"
+
+    def ready(self):
+        start_binance_klines()
