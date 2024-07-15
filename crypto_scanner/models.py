e8b968012a65986c51baf931f40b3f7ab35396df
aprs00 <ploutos@DESKTOP-LQ6OCIF>
2024-07-15 19:43:17 +0200

    Add historical z scores model

--- crypto_scanner/models.py
+++ crypto_scanner/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.db.models import UniqueConstraint
 
-# class ZScoreHistorical(models.Model):
-
 
 class Ticker(models.Model):
     name = models.CharField(max_length=20, null=True)
@@ -13,6 +11,30 @@ class Ticker(models.Model):
 
     class Meta:
         db_table = "crypto_scanner_binance_spot_tickers"
+
+
+class ZScoreHistorical(models.Model):
+    ticker_name = models.ForeignKey(Ticker, on_delete=models.CASCADE, default=1)
+    ticker_quote = models.ForeignKey(
+        Ticker, on_delete=models.CASCADE, related_name="quote_ticker", default=1
+    )
+    volume_z_score = models.FloatField()
+    price_z_score = models.FloatField()
+    trades_z_score = models.FloatField()
+    calculated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["calculated_at"]
+        db_table = "crypto_scanner_z_score_historical"
+        constraints = [
+            UniqueConstraint(
+                fields=["ticker_name", "ticker_quote", "calculated_at"],
+                name="unique_ticker_start_time",
+            ),
+        ]
+
+    def __str__(self):
+        return f"{self.ticker_name} - {self.calculated_at}"
 
 
 class BinanceSpotKline5m(models.Model):
