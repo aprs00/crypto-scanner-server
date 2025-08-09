2aef9c0474882a5bd5ad60bf46d107499e732617
aprs00 <aprskalo00@gmail.com>
2025-08-09 19:09:38 +0200

    Use exchange symbols table

--- exchange_connections/selectors.py
+++ exchange_connections/selectors.py
@@ -1,15 +1,13 @@
-from exchange_connections.models import Kline1m
+from exchange_connections.models import Kline1m, Symbol
 
 
 def get_exchange_symbols(exchange="binance", contract_type="perpetual"):
-    """Return a list of unique symbols from Kline1m where exchange is 'binance'."""
     return list(
-        Kline1m.objects.filter(
+        Symbol.objects.filter(
             exchange__name=exchange, contract_type__name=contract_type
         )
-        .order_by("symbol__name")
-        .distinct("symbol__name")
-        .values_list("symbol__name", flat=True)
+        .order_by("name")
+        .values_list("name", flat=True)
     )
 
 
