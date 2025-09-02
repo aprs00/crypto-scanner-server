4fdd6b4b3a1a8a66b534269ce3d72feb3fd06521
aprs00 <aprskalo00@gmail.com>
2025-09-02 23:22:41 +0200

    Remove ticker colors

--- zscore/utils.py
+++ zscore/utils.py
@@ -1,20 +1,11 @@
-from core.constants import ticker_colors
-
-
 def format_z_score_matrix_response(data: dict, x_axis: str, y_axis: str):
     return [
         {
-            "type": "scatter",
-            "name": symbol,
+            "symbol": symbol,
             "data": [
-                [
-                    round(data[symbol][x_axis], 2),
-                    round(data[symbol][y_axis], 2),
-                ]
+                round(data[symbol][x_axis], 2),
+                round(data[symbol][y_axis], 2),
             ],
-            "color": ticker_colors[i],
-            "symbolSize": 20,
-            "emphasis": {"scale": 1.6},
         }
-        for i, symbol in enumerate(list(data.keys()))
+        for symbol in list(data.keys())
     ]
