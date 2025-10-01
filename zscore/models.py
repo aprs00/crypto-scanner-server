f80c6effc93dca50d7f9283a192d7364d943cd6c
aprs00 <aprskalo00@gmail.com>
2025-10-01 22:35:38 +0200

    Remove unnecessary tables

--- zscore/models.py
+++ zscore/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import UniqueConstraint
 
-from exchange_connections.models import Ticker
 from exchange_connections.models import Symbol
 
 
@@ -29,27 +28,3 @@ class ZScoreHistory(models.Model):
                 include=["price", "volume", "trades", "hours", "symbol_id"],
             )
         ]
-
-
-class ZScoreHistorical(models.Model):
-    ticker_name = models.ForeignKey(Ticker, on_delete=models.CASCADE, default=1)
-    ticker_quote = models.ForeignKey(
-        Ticker, on_delete=models.CASCADE, related_name="zscore_quote_ticker", default=1
-    )
-    volume_z_score = models.FloatField()
-    price_z_score = models.FloatField()
-    trades_z_score = models.FloatField()
-    calculated_at = models.DateTimeField(auto_now_add=True, db_index=True)
-
-    class Meta:
-        ordering = ["calculated_at"]
-        db_table = "crypto_scanner_z_score_historical"
-        constraints = [
-            UniqueConstraint(
-                fields=["ticker_name", "ticker_quote", "calculated_at"],
-                name="unique_zscore_ticker_start_time",
-            ),
-        ]
-
-    def __str__(self):
-        return f"{self.ticker_name} - {self.calculated_at}"
