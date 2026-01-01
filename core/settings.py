116c2489f8de0c6132e8a020c9680c7ce3f5c5cc
aprs00 <aprskalo00@gmail.com>
2026-01-01 22:28:00 +0100

    Add core to installed apps

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "core",
     "exchange_connections",
     "correlations",
     "zscore",
